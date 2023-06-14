19298c17afcb8460683e556e13ff7225b9774d34
Jaime <jaime251102@gmail.com>
2023-06-14 01:30:43 -0400

    change json format

--- MembretesApp/Program.cs
+++ MembretesApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MembretesApp;
 using Newtonsoft.Json;
 
-string rutaArchivoJson = "D:\\Apps\\MembretesApp\\MembretesApp\\json\\letters.json";
+string rutaArchivoJson = "json\\letters.json";
 
 string json = File.ReadAllText(rutaArchivoJson);
 
@@ -11,11 +11,7 @@ lettersSet? letterset = JsonConvert.DeserializeObject<lettersSet>(json);
 var msg = Console.ReadLine();
 string msg_codificado = "";
 
-string linea1 = "";
-string linea2 = "";
-string linea3 = "";
-string linea4 = "";
-string linea5 = "";
+string[] lineas = { "", "", "", "", "" };
 
 
 
@@ -24,25 +20,23 @@ foreach (var letra in msg.ToUpper())
     try
     {
         var current = letterset.letters.FirstOrDefault(le => le.l == letra);
-        linea1 += current.l1;
-        linea2 += current.l2;
-        linea3 += current.l3;
-        linea4 += current.l4;
-        linea5 += current.l5;
+        for (int i = 0; i < 5; i++)
+        {
+            lineas[i] += current.lines[i];
 
+        }
     }
     catch (Exception)
     {
-
         Console.WriteLine("xD");
     }
 }
 
-msg_codificado += linea1 + "\n";
-msg_codificado += linea2 + "\n";
-msg_codificado += linea3 + "\n";
-msg_codificado += linea4 + "\n";
-msg_codificado += linea5 + "\n";
+foreach (var linea in lineas)
+{
+    msg_codificado += linea + "\n";
+}
+
 
 Console.WriteLine(msg_codificado);
 
--- MembretesApp/letter.cs
+++ MembretesApp/letter.cs
@@ -3,11 +3,7 @@
     public class letter
     {
         public char l { get; set; }
-        public string l1 { get; set; } = string.Empty;
-        public string l2 { get; set; } = string.Empty;
-        public string l3 { get; set; } = string.Empty;
-        public string l4 { get; set; } = string.Empty;
-        public string l5 { get; set; } = string.Empty;
+        public string[]? lines { get; set; }
 
 
     }
