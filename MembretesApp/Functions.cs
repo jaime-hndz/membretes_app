86ba1566a034ff5c50663dd2b545c4626d186826
Jaime <jaime251102@gmail.com>
2023-07-23 19:18:41 -0400

    optimize code

---
+++ MembretesApp/Functions.cs
@@ -0,0 +1,80 @@
+﻿using MembretesApp.entity;
+
+public class Functions
+{
+    public static string CodificarMensaje(string mensaje, lettersSet? letterset, frame frame)
+    {
+        string[] lineas = { "", "", "", "", "", "", "" };
+
+        for (int a = 0; a < mensaje.Length; a++)
+        {
+            var letra = mensaje.ToUpper()[a];
+            try
+            {
+                var current = letterset.letters.FirstOrDefault(le => le.l == letra);
+                for (int i = 0; i < 5; i++)
+                {
+                    lineas[i + 1] += current.lines[i];
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("xD");
+            }
+        }
+
+        int size = 0;
+        size = lineas[1].Length;
+
+        for (int i = 0; i < size; i++)
+        {
+            lineas[0] += frame.borderX;
+            lineas[6] += frame.borderX;
+        }
+
+        for (int i = 0; i < lineas.Length; i++)
+        {
+            if (i == 0 || i == 6)
+            {
+                lineas[i] = frame.corner + frame.borderX + lineas[i] + frame.borderX + frame.corner;
+            }
+            else
+            {
+                lineas[i] = frame.borderY + " " + lineas[i] + " " + frame.borderY;
+            }
+        }
+
+        string msg_codificado = string.Join("\n", lineas);
+        return msg_codificado;
+    }
+
+    public static void GuardarMensajeEnArchivo(string mensaje)
+    {
+        string msg_to_file = "";
+
+        foreach (var linea in mensaje.Split('\n'))
+        {
+            string nuevaLinea = string.Empty;
+            foreach (var letra in linea)
+            {
+                if (letra == '\\')
+                {
+                    nuevaLinea += letra.ToString() + letra.ToString();
+                }
+                else
+                {
+                    nuevaLinea += letra.ToString();
+                }
+            }
+            msg_to_file += "cout<<\"" + nuevaLinea + "\"<<endl;\n";
+        }
+
+        TextCopy.ClipboardService.SetText(msg_to_file);
+
+        using (StreamWriter sw = new StreamWriter("membrete.txt"))
+        {
+            sw.WriteLine(msg_to_file);
+        }
+    }
+}
+
--- MembretesApp/Program.cs
+++ MembretesApp/Program.cs
@@ -2,98 +2,21 @@
 using Newtonsoft.Json;
 
 string rutaArchivoJson = "json\\letters.json";
-
 string json = File.ReadAllText(rutaArchivoJson);
-
 lettersSet? letterset = JsonConvert.DeserializeObject<lettersSet>(json);
 
-
 string rutaArchivoJson2 = "json\\frames.json";
-
 string json2 = File.ReadAllText(rutaArchivoJson2);
-
 framesSet? framesSet = JsonConvert.DeserializeObject<framesSet>(json2);
 
 frame fra = framesSet.frames.First();
 
+Console.WriteLine("Ingresa un mensaje:");
 var msg = Console.ReadLine();
-string msg_codificado = "";
+string msg_codificado = Functions.CodificarMensaje(msg, letterset, fra);
 
-string[] lineas = { "", "", "", "", "", "", "" };
-
-
-
-foreach (var letra in msg.ToUpper())
-{
-    try
-    {
-        var current = letterset.letters.FirstOrDefault(le => le.l == letra);
-        for (int i = 0; i < 5; i++)
-        {
-            lineas[i+1] += current.lines[i];
-
-        }
-    }
-    catch (Exception)
-    {
-        Console.WriteLine("xD");
-    }
-}
-
-int size = 0;
-size = lineas[1].Length;
-
-for (int i = 0; i < size; i++)
-{
-    lineas[0] += fra.borderX;
-    lineas[6] += fra.borderX;
-}
-
-for(int i = 0; i < lineas.Length; i++)
-{
-    if (i == 0 || i == 6)
-    {
-        lineas[i] = fra.corner + fra.borderX + lineas[i] + fra.borderX + fra.corner;
-    }
-    else
-    {
-        lineas[i] = fra.borderY + " " + lineas[i] + " " + fra.borderY;
-    }
-    
-}
-
-
-foreach (var linea in lineas)
-{
-    msg_codificado += linea + "\n";
-}
-
-string msg_to_file = "";
-
-foreach (var linea in lineas)
-{
-    string nuevaLinea = string.Empty;
-    foreach (var letra in linea)
-    {
-        if (letra == '\\')
-        {
-            nuevaLinea += letra.ToString() + letra.ToString();
-        }
-        else
-        {
-            nuevaLinea += letra.ToString();
-        }
-    }
-    msg_to_file += "cout<<\"" + nuevaLinea + "\"<<endl;\n";
-}
-
-TextCopy.ClipboardService.SetText(msg_to_file);
 Console.WriteLine(msg_codificado);
-
-StreamWriter sw = new StreamWriter("membrete.txt");
-sw.WriteLine(msg_to_file);
-
-sw.Close();
+Functions.GuardarMensajeEnArchivo(msg_codificado);
 
 Console.Write("\nPresiona cualquier tecla para continuar...");
 Console.ReadKey();
